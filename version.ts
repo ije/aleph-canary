66a5758206d27e6d961ab5cfb5b0e2a7df0b3577
X <i@jex.me>
2022-02-22 05:36:57 +0800

    0.0.5

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "0.0.4";
+export const VERSION = "0.0.5";
 
 /** minimum supported deno version */
 export const minDenoVersion = "1.18.2";
