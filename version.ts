18c01a78a95951bbe1d4d5c45df02d5ad034b7b9
X <i@jex.me>
2022-02-24 03:08:11 +0800

    0.0.16

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "0.0.15";
+export const VERSION = "0.0.16";
 
 /** whether is canary version */
 export const isCanary = true;
